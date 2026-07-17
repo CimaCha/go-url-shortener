b629d5599d74292f08e617f69fb17d625c4b1b95
Maksim Cha <mscha@avito.ru>
2026-07-17 09:32:48 +0300

    add main and client for testing

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/CimaCha/go-url-shortener/internal/handler/post_shorten_url"
+	"github.com/CimaCha/go-url-shortener/internal/repository"
+	"github.com/CimaCha/go-url-shortener/internal/service"
+	"net/http"
+)
+
+func main() {
+	ctx := context.Background()
+
+	redisClient := repository.NewRedisClient(`localhost:6379`)
+
+	shortenUrlService := service.NewService(redisClient)
+
+	shortenUrlHandler := post_shorten_url.NewShortenUrlHandler(ctx, shortenUrlService)
+
+	mux := http.NewServeMux()
+	mux.Handle(`/`, shortenUrlHandler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
