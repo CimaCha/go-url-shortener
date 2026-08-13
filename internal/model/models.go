54e7bd099ea933bcf02951ed2186370ddcd91590
Maksim Cha <mscha@avito.ru>
2026-08-13 15:30:02 +0300

    add file storage

--- internal/model/models.go
+++ internal/model/models.go
@@ -7,3 +7,9 @@ type ShortenURLRequest struct {
 type ShortenURLResponse struct {
 	Result string `json:"result,omitempty"`
 }
+
+type FileRecord struct {
+	UUID        string `json:"uuid,omitempty"`
+	ShortUrl    string `json:"short_url,omitempty"`
+	OriginalUrl string `json:"original_url,omitempty"`
+}
