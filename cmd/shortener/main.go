84d519670303f47ecf08583bb5dcb094926ff9cf
Maksim Cha <mscha@avito.ru>
2026-08-13 15:30:49 +0300

    add file storage for service

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,40 +1,47 @@
 package main
 
 import (
-	apishortenurl "github.com/CimaCha/go-url-shortener/internal/handler/post-api-shorten-url"
-	"net/http"
-	"os"
-
+	"fmt"
 	"github.com/CimaCha/go-url-shortener/internal/config"
 	"github.com/CimaCha/go-url-shortener/internal/handler/get-full-url"
+	apishortenurl "github.com/CimaCha/go-url-shortener/internal/handler/post-api-shorten-url"
 	"github.com/CimaCha/go-url-shortener/internal/handler/post-shorten-url"
 	"github.com/CimaCha/go-url-shortener/internal/logger"
-	"github.com/CimaCha/go-url-shortener/internal/repository"
+	"github.com/CimaCha/go-url-shortener/internal/repository/file"
 	shortenerrouter "github.com/CimaCha/go-url-shortener/internal/router"
 	"github.com/CimaCha/go-url-shortener/internal/service"
 	"go.uber.org/zap"
+	"net/http"
+	"os"
 )
 
 func main() {
 	if err := logger.Initialize("debug"); err != nil {
 		panic(err)
 	}
-	defer func() {
-		_ = logger.Log.Sync()
-	}()
-
-	cfg, err := config.New()
-	if err != nil {
-		logger.Log.Error("Error to parse config", zap.Error(err))
+	if err := run(); err != nil {
+		logger.Log.Error("application stopped", zap.Error(err))
 		_ = logger.Log.Sync()
 		os.Exit(1)
+	}
+	_ = logger.Log.Sync()
+}
+
+func run() error {
+	cfg, err := config.New()
+	if err != nil {
+		return fmt.Errorf("parse config: %w", err)
 	}
 
 	logger.Log.Info("address of service", zap.String("address", cfg.Address))
 	logger.Log.Info("basic address for short url", zap.String("basic url", cfg.BasicShortenAddress))
+	logger.Log.Info("file storage path", zap.String("path", cfg.FilePath))
 
-	storage := repository.NewMemoryURLStorage()
-	shortenURLService := service.NewService(storage)
+	fileStorage, err := file.NewFileStorage(cfg.FilePath)
+	if err != nil {
+		return fmt.Errorf("open file storage: %w", err)
+	}
+	shortenURLService := service.NewService(fileStorage)
 
 	shortenURLHandler := shortenurl.NewShortenURLHandler(shortenURLService, cfg.BasicShortenAddress)
 	apiShortenURLHandler := apishortenurl.NewApiShortenURLHandler(shortenURLService, cfg.BasicShortenAddress)
@@ -48,6 +55,7 @@ func main() {
 	err = http.ListenAndServe(cfg.Address, router)
 	if err != nil {
 		logger.Log.Error("HTTP server stopped", zap.Error(err))
-		os.Exit(1)
+		return err
 	}
+	return nil
 }
